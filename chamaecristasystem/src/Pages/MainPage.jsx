ac6e64e7fab119fee207f365f06ab1d71af2e80a
yazetsepu <yazet.sepulveda@upr.edu>
2022-10-26 12:14:49 -0400

    Added test commit

--- chamaecristasystem/src/Pages/MainPage.jsx
+++ chamaecristasystem/src/Pages/MainPage.jsx
@@ -5,6 +5,7 @@ class MainPage {
         return (
             <div>
                 <h1>Test Page</h1>
+                <h1>Test Page2</h1>
                 <button></button>
             </div>
         );
