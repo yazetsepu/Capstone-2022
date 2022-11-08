a327892da3e7e085b193ca57282fdf8daccf7624
DavidOrtiz14 <david.ortiz14@upr.edu>
2022-11-07 20:56:39 -0400

    MinimalAPI Entity Framework
    DBContext and database contructors, and migrations, models and

---
+++ Arduino/API_Database/MinimalAPIPostgresSqlCSS/Data/CSSDb.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIPostgresSqlCSS.Models;
+
+namespace MinimalAPIPostgresSqlCSS.Data
+{
+    public class CSSDb :DbContext
+    {
+        public CSSDb(DbContextOptions<CSSDb>options) :base(options)
+        {
+
+        }
+        public DbSet<Admins> Admins => Set<Admins>();
+        public DbSet<Commands> Commands => Set<Commands>();
+        public DbSet<EnvironmentalData> EnvironmentalData => Set<EnvironmentalData>();
+        public DbSet<Logs> Logs => Set<Logs>();
+        public DbSet<Pictures> Pictures => Set<Pictures>();
+
+        
+    }
+}
