5fabb92a2cdfdcf3bc23464723fa2c252ba5844c
yazetsepu <yazet.sepulveda@upr.edu>
2022-11-08 10:42:47 -0400

    Initial Connection with Azure

--- chamaecristasystemui/src/Components/DataTable.jsx
+++ chamaecristasystemui/src/Components/DataTable.jsx
@@ -1,15 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import '../styles/DataTable.css';
 import Table from 'react-bootstrap/Table';
 import { envData } from '../Util/dummyData'
 
 function BasicTable() {
+
+    const [envData, setEnvData] = useState([])
+    const [picture, setPicture] = useState([])
+    
+    const fetchData = async () => {
+        const response = await fetch("https://cssrumapi.azurewebsites.net/EnvironmentalDataAll")
+        const data = await response.json()
+        setEnvData(data)
+        
+        // const picId = envData.picturesId
+        // const picResponse = await fetch("https://cssrumapi.azurewebsites.net/Pictures/" + picId)
+        // const picData = await response.json()
+        // setPicture(picData)
+      }
+    
+      useEffect(() => {
+        fetchData()
+
+      }, [])
+
     return (
         <Table striped bordered hover className='table'>
             <thead>
                 <tr>
                     <th>#</th>
                     <th>Timestamp</th>
-                    <th>Leaf Status</th>
+                    {/* <th>Leaf Status</th> */}
+                    <th>Water Level</th>
                     <th>Temperature (C)</th>
                     <th>Soil Moisture (%)</th>
                     <th>Air Humidity (%)</th>
@@ -18,12 +40,13 @@ function BasicTable() {
             <tbody>
                 {envData.map((data, key) => (
                 <tr key = {key}>
-                    <td>{data.id}</td>
-                    <td>{data.timestamp}</td>
-                    <td>{data.leafstatus}</td>
+                    <td>{data.entry_Id}</td>
+                    <td>{data.times_tamps}</td>
+                    {/* <td>{data.pictures.classification_id}</td> */}
+                    <td>{data.water_level}</td>
                     <td>{data.temperature}</td>
-                    <td>{data.soilmoisture}</td>
-                    <td>{data.airhumidity}</td>
+                    <td>{data.soil_moisture}</td>
+                    <td>{data.humidity}</td>
                 </tr>
                 ))}
             </tbody>
